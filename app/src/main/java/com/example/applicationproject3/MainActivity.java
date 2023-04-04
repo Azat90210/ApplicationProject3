49f3921172c39442470f817648d5273f43fe23b0
Mukhs <azat_market@mail.ru>
2023-04-04 19:40:46 +0300

    Edit1

--- app/src/main/java/com/example/applicationproject3/MainActivity.java
+++ app/src/main/java/com/example/applicationproject3/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
 //        binding.fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
